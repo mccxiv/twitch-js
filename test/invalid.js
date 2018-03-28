bf639c2c960a7f56c34da61ca15420e71f0ffeec
mccxiv <adr.stella@gmail.com>
2018-03-28 22:33:38 +0800

    Singlify and fix tests

--- test/invalid.js
+++ test/invalid.js
@@ -3,7 +3,6 @@ const tmi = require('../src/index.js');
 const tests = [
   'FOO',
   ':tmi.twitch.tv FOO',
-  ':tmi.twitch.tv NOTICE #schmoopiie : FOO',
   ':jtv FOO',
   ':schmoopiie!schmoopiie@schmoopiie.tmi.twitch.tv FOO',
 ];
