04c2885b13d15c64748450503591f5a69885ffe1
mccxiv <adr.stella@gmail.com>
2018-08-12 10:43:33 +0800

    Fix emote parsing in outgoing messages

--- src/parser.js
+++ src/parser.js
@@ -73,12 +73,13 @@ module.exports = {
   // Parse regex emotes..
   emoteRegex: function emoteRegex(msg, code, id, obj) {
     const space = /\S+/g;
-    const regex = new RegExp(`(\\b|^|\s)${_.unescapeHtml(code)}(\\b|$|\s)'`);
+    const regex = new RegExp(`(\\b|^|\s)${_.unescapeHtml(code)}(\\b|$|\s)`);
     let match;
 
     // Check if emote code matches using RegExp and push it to the object..
     while ((match = space.exec(msg)) !== null) {
-      if (regex.test(match[0]) && obj[id]) {
+      if (regex.test(match[0])) {
+        if (!obj[id]) obj[id] = [];
         obj[id].push([match.index, space.lastIndex - 1]);
       }
     }
